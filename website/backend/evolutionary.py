3a77e34fd13f127a37358a0b51c5a4d4dd5afef9
Vani-lla <igorkowaczyk24@gmail.com>
2025-12-08 19:58:24 +0100

    latex

--- website/backend/evolutionary.py
+++ website/backend/evolutionary.py
@@ -1,6 +1,5 @@
 import numpy as np
 from backend.helpers import *
-import matplotlib.pyplot as plt
 from backend.linear_solver import solve_schedule
 from time import time
 
@@ -8,12 +7,10 @@ np.set_printoptions(precision=3, suppress=True)
 
 
 def run_evolutionary_process(generations: int, req_set_id: int):
-    # Fetch the specified requirement set
     REQ_SET = RequirementSet.objects.get(id=req_set_id)
     REQUIREMENTS = Requirement.objects.filter(req_set=REQ_SET)
     VALIDATION_HOURS = list(map(lambda req: req.hours, REQUIREMENTS))
 
-    # Prefetch teachers and student groups
     TEACHERS = list(Teacher.objects.filter(pool=REQ_SET.teacher_pool))
     TEACHER_AVAILABILITY = np.array(
         [
@@ -28,7 +25,6 @@ def run_evolutionary_process(generations: int, req_set_id: int):
     )
     STUDENT_GROUPS = list(StudentGroup.objects.filter(pool=REQ_SET.group_pool))
 
-    # Generate blocks and evaluate population
     print(len(REQUIREMENTS))
 
     start = time()
@@ -69,23 +65,42 @@ def run_evolutionary_process(generations: int, req_set_id: int):
     ll = []
     for k, v in teacher_day_hours(BLOCK_LIST, REQ_SET, best_specimen).items():
         # print(sum(v))
-        ll.append(sum(v))
-    l = np.array(list(teacher_day_hours(BLOCK_LIST, REQ_SET, best_specimen).values())).flatten()
-    for x in np.unique(l):
-        print(x, np.sum(l==x))
-    print("--")
-    for x in range(max(ll) + 1):
-        print(f"({x}, {ll.count(x)})")
+        # ll.append(sum(v))
+        zeros = np.where(v == 0)
+        v = np.array(v, dtype=np.int64)
+        # a1: np.ndarray = alphas[0] * (2 - np.abs(a1 - 7)) / len(teachers)
+        a1 = (-((7 - v) ** 2) + 2)
+        print(a1, v)
+        a1[zeros] = 0.0
+        if sum(v) > 0:
+            ll.append(a1.sum())
+    # print(list(int(x) for x in ll))
+    # return
+    # l = np.array(list(teacher_day_hours(BLOCK_LIST, REQ_SET, best_specimen).values())).flatten()
+    # for x in np.unique(l):
+    #     print(x, np.sum(l==x))
+    # print("--")
+    # for x in range(max(ll) + 1):
+    #     print(f"({x}, {ll.count(x)})")
         
     print("------ xd")
     l = []
     ll = []
     for k, v in group_day_lessons(BLOCK_LIST, REQ_SET, best_specimen).items():
-        ll.append(sum(v))
-        l.extend(v)
-    print("--")
-    for x in range(max(ll)):
-        print(f"({x}, {ll.count(x)})")
+        # print(sum(v))
+        # ll.append(sum(v))
+        v = np.array(v, dtype=np.int64)
+        # a1: np.ndarray = alphas[0] * (2 - np.abs(a1 - 7)) / len(teachers)
+        a1 = (-((7 - v) ** 2) + 2)
+        print(a1, v)
+        if sum(v) > 0:
+            ll.append(a1.sum())
+    print(list(int(x) for x in ll))
+    print(ll)
+        
+    # print("--")
+    # for x in range(max(ll)):
+    #     print(f"({x}, {ll.count(x)})")
         
     print("--")
     l = np.array(l)
